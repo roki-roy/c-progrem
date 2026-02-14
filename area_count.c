236a1a3859aec6890f02adffbf37aa4202027666
roki-roy <rokimg86@gmail.com>
2026-02-15 02:01:22 +0600

    uplode

---
+++ area_count.c
@@ -0,0 +1,15 @@
+/*
+> first input int valu
+> multipling input valu
+> area count 
+*/
+#include<stdio.h>
+
+int main(){
+    int place;
+    printf("Enter place : ");
+    scanf("%d", &place);
+
+    printf("the area : %d", place*place);
+    return 0;
+}
