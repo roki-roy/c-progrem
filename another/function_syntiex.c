1abf844798e42bb1643dd5b220aab132019072d2
roki-roy <rokimg86@gmail.com>
2026-03-15 19:22:51 +0600

    function basic syntax

---
+++ another/function_syntiex.c
@@ -0,0 +1,51 @@
+/*
+Formet for function
+
+syntax - 1
+
+Function prototype
+
+void printHellow();  > tell the compiler
+
+syntax - 2
+sapos
+
+Function Call
+
+int main(){
+    printfHello();
+    return 0;
+}                    > Use the work
+
+Syntax - 3
+
+Function Definition
+
+void printHello(){
+   printf(" ");      > Do the work
+}
+
+*/
+
+//basic Example
+
+#include<stdio.h>
+
+//function decleration/prototype
+void printHello();
+
+//function call
+int main(){
+    printHello();
+    printHello();
+    printHello();
+    printHello();
+    return 0;
+}
+
+//function Defination
+
+void printHello(){
+    printf("Hellow world!\n");
+    printf("wonderfull pepole\n");
+}
